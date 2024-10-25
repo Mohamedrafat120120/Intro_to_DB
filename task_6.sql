abc777070ce12ab831cc932d61b07c6be515dab8
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-10-26 01:17:14 +0300

    6. Populating Your Database with Your More Data

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE alx_book_store;
 USE alx_book_store;
-CREATE TABLE Customer(customer_id INT PRIMARY KEY,customer_name VARCHAR(50) NOT NULL,email VARCHAR(50) NOT NULL,address VARCHAR(50) NOT NULL);
-INSERT INTO Customer(customer_id,customer_name,email,address)VALUES(2,`Blessing Malik`,`bmalik@sandtech.com`,`124 Happiness  Ave.`),(3,`Obed Ehoneah`,`eobed@sandtech.com`,`125 Happiness  Ave.`),(4,`Nehemial Kamolu`,`nkamolu@sandtech.com`,`126 Happiness  Ave.`);
+CREATE TABLE customer(customer_id INT PRIMARY KEY,customer_name VARCHAR(50) NOT NULL,email VARCHAR(50) NOT NULL,address VARCHAR(50) NOT NULL);
+INSERT INTO customer(customer_id,customer_name,email,address)VALUES(2,`Blessing Malik`,`bmalik@sandtech.com`,`124 Happiness  Ave.`),(3,`Obed Ehoneah`,`eobed@sandtech.com`,`125 Happiness  Ave.`),(4,`Nehemial Kamolu`,`nkamolu@sandtech.com`,`126 Happiness  Ave.`);
