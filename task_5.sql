666b0988747126528f629b85f4d83a7b25c24f2f
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-10-26 01:08:07 +0300

    4. Full description

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE alx_book_store;
 USE alx_book_store;
-CREATE TABLE Customers( customer_id=1 INT PRIMARY KEY,customer_name VARCHAR(50) NOT NULL,email VARCHAR(100) UNIQUE NOT NULL,address VARCHAR(50) NOT NULL);
-INSERT INTO Customers(customer_id,customer_name,email,address) VALUES(1,"Cole Baidoo","cbaidoo@sandtech.com","123 Happiness Ave.");
+CREATE TABLE Customer( customer_id=1 INT PRIMARY KEY,customer_name VARCHAR(50) NOT NULL,email VARCHAR(100) UNIQUE NOT NULL,address VARCHAR(50) NOT NULL);
+INSERT INTO Customer(customer_id,customer_name,email,address) VALUES(1,"Cole Baidoo","cbaidoo@sandtech.com","123 Happiness Ave.");
