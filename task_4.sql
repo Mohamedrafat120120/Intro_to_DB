6d02551e8037fd3b1e08a66e1fc622bbd1add357
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-10-26 00:43:11 +0300

    4. Full description

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE alx_book_store;
 USE alx_book_store;
-CREATE TABLE books;
+CREATE TABLE books(id INT Primary KEY,name VARCHAR(50) NOT NULL,type VARCHAR(50) NOT NULL);
 SELECT * FROM books;
